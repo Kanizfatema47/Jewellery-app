779ec8afdeeff91cc641831278cea5df5006dd2e
Kaniz Fatema <kaniz.fatema@bitopibd.com>
2023-02-18 00:03:25 +0600

    contact us , testomonials, products, signin, products by choice done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './pages/HomePage/Home';
 
 function App() {
   return (
     <div className="">
-     <h1>React</h1>
+     <Home/>
     </div>
   );
 }
---
+++ src/pages/HomePage/Home.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Signin from '../../components/Auth/Signin';
+import Footer from '../../shared/Footer/Footer';
+import Navbar from '../../shared/Header/Navbar';
+import UpNev from '../../shared/Header/UpNev';
+import AllProducts from '../AllCategories/AllProducts';
+import Banner from './Banner';
+import ByChoice from './ByChoice';
+import Testimonials from './Testimonials';
+
+const Home = () => {
+    return (
+        <div>
+            <div className="bg-hero bg-no-repeat bg-cover bg-center h-screen" >
+            <UpNev/>
+            <Navbar/>
+            <Banner/>
+        </div>
+        <ByChoice/>
+        <AllProducts/>
+        <Testimonials/>
+        <Signin/>
+          <Footer/>
+        </div>
+    );
+};
+
+export default Home;
